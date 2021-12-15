1ac664ad3064751048ce690b547666f3d58937c2
Jail3 <67543959+Jail3@users.noreply.github.com>
2021-12-15 19:40:05 +0630

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,32 @@
-
 function serveMenu(site_detail) {
     document.querySelector('#menu-btn').onclick = function() {
-        alert('Clicked on menu');
+        createMenu(site_detail.site_name.url, site_detail.site_name.tc);
+        createMenu(site_detail.site_home.url, site_detail.site_home.tc);
+        createMenu(site_detail.products.url, site_detail.products.tc);
+        let rmMenu = document.createElement('button');
+        rmMenu.setAttribute('id','rm-menu-btn');
+        rmMenu.textContent = 'Close Menu';
+        const menuContainer = document.querySelector('#menu-container');
+        menuContainer.appendChild(rmMenu);
+        removeMenu();
+    }
+}
+
+function createMenu(url, tc) {
+    let menuContainer = document.querySelector('#menu-container');
+    let menuItem = document.createElement('a');
+    menuItem.setAttribute('href',url);
+    menuItem.setAttribute('class','menu-item');
+    menuItem.textContent = tc;
+    menuContainer.appendChild(menuItem);
+}
+
+function removeMenu() {
+    const menuContainer = document.querySelector('#menu-container');
+    document.querySelector('#rm-menu-btn').onclick = function() {
+        while (menuContainer.firstChild) {
+            menuContainer.removeChild(menuContainer.lastChild);
+        }
     }
 }
 export {serveMenu}
