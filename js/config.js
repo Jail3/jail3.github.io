f86eeb8e296cca45c3adb6901f577ec79b562ea2
Jail3 <67543959+Jail3@users.noreply.github.com>
2021-12-15 19:09:40 +0630

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,10 @@
 
+let site_detail = {
+    'site_name': 'SpringMoni',
+    'tagline': 'A site script for static website with dynamic data',
+    'site_home': 'https://lovelymyanmar.github.io',
+    'blog_home': 'https://lovalymyanmar.github.io/springmoni',
+    'products': 'products.html'
+}
+
+export {site_detail}
